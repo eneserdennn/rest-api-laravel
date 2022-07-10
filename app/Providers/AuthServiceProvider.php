fd3f29f9e5f3484d025506a02673dd63719c2ff6
Enes Erden <admin@eneserden.com>
2022-07-10 15:00:04 +0300

    breeze/react imported

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -13,7 +13,7 @@ class AuthServiceProvider extends ServiceProvider
      * @var array<class-string, class-string>
      */
     protected $policies = [
-        // 'App\Models\Model' => 'App\Policies\ModelPolicy',
+        // 'Home\Models\Model' => 'Home\Policies\ModelPolicy',
     ];
 
     /**
