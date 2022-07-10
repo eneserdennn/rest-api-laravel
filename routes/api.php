477a88dfee8965e51628ef3c4d8f4c2c92873473
Enes Erden <admin@eneserden.com>
2022-07-10 11:34:15 +0300

    basic crud operations and search added

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+
+use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,9 +16,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/products', function (){
-    return 'products';
-});
+Route::resource('products', ProductController::class);
+Route::get('products/search/{name}', [ProductController::class, 'search']);
+
+
+//Route::get('/products', [ProductController::class, 'index']);
+//Route::post('/products', [ProductController::class, 'store']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
